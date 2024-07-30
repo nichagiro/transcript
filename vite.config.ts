7225dda78e77f5f7d695de14267a7fd1cab7007a
Nicolas <nicolas.chamorro@imbanacocomco.onmicrosoft.com>
2024-07-30 17:51:24 -0500

    CONFIG BUILD CSS Y JS

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(() => {
   return {
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: 'src/main.tsx'
+        },
+        output: {
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name].js',
+          assetFileNames: '[name][extname]',
+        },
+      },
+    },
   }
 })
