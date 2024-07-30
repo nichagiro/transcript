455d95670d64b74a65a80a55edc8129b6a3700ff
Nicolas <nicolas.chamorro@imbanacocomco.onmicrosoft.com>
2024-07-30 14:34:46 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(() => {
   return {
-    // base: env.mode === "production" ? "/transcript" : "/",
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: 'src/main.tsx'
-        },
-        output: {
-          entryFileNames: '[name].js',
-          chunkFileNames: '[name].js',
-          assetFileNames: '[name][extname]',
-        },
-      },
-    },
   }
 })
