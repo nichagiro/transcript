7b48cfcb1d8b1159719aff52be1514cf8a2da009
Nicolas <nicolas.chamorro@imbanacocomco.onmicrosoft.com>
2024-08-22 11:10:20 -0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
+    base: mode === "production" ? "/testMicroPhone" : "",
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: 'src/main.tsx'
-        },
-        output: {
-          entryFileNames: '[name].js',
-          chunkFileNames: '[name].js',
-          assetFileNames: '[name][extname]',
-        },
-      },
-    },
   }
 })
