dad17968950bd08489fe50a9d06e283efa2fd123
Nicolas Chamorro <nichagiro@gmail.com>
2024-10-30 14:15:43 -0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: 'src/main.tsx'
-        },
-        output: {
-          entryFileNames: '[name].js',
-          chunkFileNames: '[name].js',
-          assetFileNames: '[name][extname]',
-        },
-      },
-    },
+    // build: {
+    //   rollupOptions: {
+    //     input: {
+    //       main: 'src/main.tsx'
+    //     },
+    //     output: {
+    //       entryFileNames: '[name].js',
+    //       chunkFileNames: '[name].js',
+    //       assetFileNames: '[name][extname]',
+    //     },
+    //   },
+    // },
   }
 })
