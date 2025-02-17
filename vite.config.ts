ba2f67780683d05d889d7b8aa87cd7ad9ae1872c
Nicolas <nicolas.chamorro@imbanacocomco.onmicrosoft.com>
2025-02-17 17:31:21 -0500

    update basePath

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === "production" ? "/testMicroPhone" : "",
+    base: mode === "production" ? "/transcripcion" : "",
     plugins: [react()],
   }
 })
